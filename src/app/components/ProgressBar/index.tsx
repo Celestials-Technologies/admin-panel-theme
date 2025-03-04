441b2ca2ebe6935f1ab313c92e1d9459e16ea1db
waqasmalik10 <waqasmalik10@hotmail.com>
2025-03-04 16:41:19 +0500

    Add Progressbar and Switch

---
+++ src/app/components/ProgressBar/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+interface ProgressBarProps {
+  progress: number;
+}
+function ProgressBar({ progress }: ProgressBarProps) {
+  return (
+    <div className="h-2.5 w-full rounded-full bg-gray-200 dark:bg-gray-700">
+      <div className="h-2.5 rounded-full bg-blue-600" style={{ width: `${progress}%` }}></div>
+    </div>
+  );
+}
+
+export default ProgressBar;
